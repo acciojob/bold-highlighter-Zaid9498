3f1ddb8cf7306aa734e4086a3f8f314f6a504249
Zaid9498 <136815199+Zaid9498@users.noreply.github.com>
2023-08-25 23:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
-    //Write your code here
-
-
+    const boldElements = document.querySelectorAll('strong');
+    
+    for (const element of boldElements) {
+        element.style.color = 'green';
+    }
 }
-
 
 function return_normal() {
-    //Write your code here
-
+    const boldElements = document.querySelectorAll('strong');
     
+    for (const element of boldElements) {
+        element.style.color = 'black';
+    }
 }
+
